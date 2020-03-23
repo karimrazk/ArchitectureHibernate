e707115577cb2021862cff2654722fb56811135b
karimrazok <50910945+karimrazk@users.noreply.github.com>
2020-03-23 13:09:09 +0100

    feat: add some issues

--- src/com/technomaker/demohibernate/DemoHibernate.java
+++ src/com/technomaker/demohibernate/DemoHibernate.java
@@ -3,20 +3,26 @@ package com.technomaker.demohibernate;
 
 import com.technomaker.demohibernate.service.GestionnaireFormation;
 import com.technomaker.demohibernate.simple.Formation;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 
 public class DemoHibernate {
 
 	public static void main(String[] args) {
-		
+
 		// 1 : instanciation objet couche service
 		GestionnaireFormation serviceGestionnaire = new GestionnaireFormation();
 		
 		// 2 : Instanciation Objet couche métier
-		Formation formation = new Formation("Hibernate");
-		
+		Formation formation = new Formation("HibernateTest");
+		Formation formation1 = new Formation("Java 8");
+		Formation formation2 = new Formation("Spring");
 		// 3 : Utilisation du service  (création de formation)
 		serviceGestionnaire.creerFormation(formation);
-		
+		serviceGestionnaire.creerFormation(formation1);
+		serviceGestionnaire.creerFormation(formation2);
+
+
 	}
 
 }
--- src/com/technomaker/demohibernate/simple/Formation.java
+++ src/com/technomaker/demohibernate/simple/Formation.java
@@ -7,10 +7,27 @@ public class Formation {
     private Long id;
     private String theme;
 
-
-    public Formation(String theme) {
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTheme() {
+        return theme;
+    }
+    public void setTheme(String theme) {
+        this.theme = theme;
     }
 
+    // Constructeur
+    public Formation(String theme) {
+        super();
+        this.theme = theme;
+    }
+
+    // Constructeur par defaut
     public Formation() {
+        super();
     }
 }
