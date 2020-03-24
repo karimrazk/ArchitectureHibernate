e3df4a931488c03a4a26d6b3267123170edc5f5c
karimrazok <50910945+karimrazk@users.noreply.github.com>
2020-03-24 12:07:56 +0100

    feat: add Entity Participant and hbm.xml file

---
+++ src/com/technomaker/demohibernate/simple/Participant.java
@@ -0,0 +1,14 @@
+package com.technomaker.demohibernate.simple;
+
+public class Participant {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Participant() {
+        super();
+    }
+
+}
